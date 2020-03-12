dd52ea5daa4c049a5e757c75f7dff5c089a565a0
iamacoderguy <iamacoderguy@gmail.com>
2020-03-12 07:22:33 +0700

    Remove some debug warnings

--- front-end-mobile/src/lib/utils/storage.ts
+++ front-end-mobile/src/lib/utils/storage.ts
@@ -3,11 +3,9 @@ const config = {
 };
 
 export const saveCredentials = (token: string) => {
-  console.warn('credentials saved!');
   config.token = token;
 };
 
 export const loadCredentials = () => {
-  console.warn(`credentials loaded! - ${config.token}`);
   return config.token;
 };
