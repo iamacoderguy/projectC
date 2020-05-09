2c8333e0b59bef3588dda67f12478f1e034d1d9b
iamacoderguy <iamacoderguy@gmail.com>
2020-05-09 11:20:47 +0700

    Change android package name

--- front-end-mobile/android/app/src/main/java/me/iamacoderguy/buzz/MainActivity.java
+++ front-end-mobile/android/app/src/main/java/me/iamacoderguy/buzz/MainActivity.java
@@ -1,4 +1,4 @@
-package com.buzz;
+package me.iamacoderguy.buzz;
 
 import com.facebook.react.ReactActivity;
 
--- front-end-mobile/android/app/src/main/java/me/iamacoderguy/buzz/MainApplication.java
+++ front-end-mobile/android/app/src/main/java/me/iamacoderguy/buzz/MainApplication.java
@@ -1,4 +1,4 @@
-package com.buzz;
+package me.iamacoderguy.buzz;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.buzz.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("me.iamacoderguy.buzz.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
