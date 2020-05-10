fafad62d6f60b0ef8810428dc9f8783e578b32f3
iamacoderguy <iamacoderguy@gmail.com>
2020-05-10 17:20:35 +0700

    Backend: morgan config

--- back-end/src/api/installer.ts
+++ back-end/src/api/installer.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
 import helloWorldController from './controllers/helloWorld';
 
 const app = express();
+const logger = morgan('common');
 
+app.use(logger);
 app.use('/api/helloWorld', helloWorldController);
 
 export default app;
