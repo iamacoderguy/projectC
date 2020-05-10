7bfe3efdcd8cf8190a0bb4c5df226ef9ed28c8f8
iamacoderguy <iamacoderguy@gmail.com>
2020-05-10 11:55:47 +0700

    Flipper and LogBox

--- front-end-mobile/index.js
+++ front-end-mobile/index.js
@@ -2,6 +2,9 @@
  * @format
  */
 
+// https://reactnative.dev/blog/2020/03/26/version-0.62#other-improvements
+require('react-native').unstable_enableLogBox();
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
