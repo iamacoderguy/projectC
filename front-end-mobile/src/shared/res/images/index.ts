957280326e26c4f9bce51f6a786dac22c41674e8
iamacoderguy <iamacoderguy@gmail.com>
2020-05-14 21:58:01 +0700

    Remove images

--- front-end-mobile/src/shared/res/images/index.ts
+++ front-end-mobile/src/shared/res/images/index.ts
@@ -1,11 +1,8 @@
 const images = {
-  avatar_default: require('./avatar_default.png'),
   dot: require('./dot.png'),
   ic_black_yellow: require('./ic_black_yellow.png'),
-  ic_close: require('./ic_close.png'),
   ic_cross_mark: require('./ic_cross_mark.png'),
   ic_exclamation_mark: require('./ic_exclamation_mark.png'),
-  ic_github: require('./ic_github.png'),
   ic_google: require('./ic_google.png'),
   ic_nav_activities: require('./ic_nav_activities.png'),
   ic_nav_buzz_activated_1: require('./ic_nav_buzz_activated_1.png'),
